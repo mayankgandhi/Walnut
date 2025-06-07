5b98acc3cff0e73319b9f488d6a091670a117606
Mayank Gandhi <mayank@radiusagent.com>
2025-06-07 16:17:52 +0530

    update project,package.swift

--- Project.swift
+++ Project.swift
@@ -1,24 +1,89 @@
 import ProjectDescription
 
+import ProjectDescription
+
+let infoPlist: InfoPlist = .extendingDefault(with: [
+    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+    "UILaunchStoryboardName~ipad": "Launch Screen-iPAD.storyboard",
+    "UILaunchScreen": "LaunchScreen.storyboard",
+    "CFBundleName": .string("$(PRODUCT_NAME)"),
+    "CFBundleIdentifier": .string("$(PRODUCT_BUNDLE_IDENTIFIER)"),
+    "CFBundleVersion": .string("$(MARKETING_VERSION)"),
+    "CFBundleShortVersionString": .string("$(CURRENT_PROJECT_VERSION)"),
+    "CFBundlePackageType": .string("$(PRODUCT_BUNDLE_PACKAGE_TYPE)"),
+    "CFBundleExecutable": .string("$(PRODUCT_NAME)"),
+    "CFBundleDisplayName": .string("$(DISPLAY_NAME)"),
+    "LSApplicationCategoryType": .string("public.app-category.health"),
+    "UISupportedInterfaceOrientations": .array([
+        .string("UIInterfaceOrientationPortrait"),
+    ]),
+    "UISupportedInterfaceOrientations~ipad": .array([
+        .string("UIInterfaceOrientationPortrait"),
+        .string("UIInterfaceOrientationPortraitUpsideDown"),
+        .string("UIInterfaceOrientationLandscapeLeft"),
+        .string("UIInterfaceOrientationLandscapeRight"),
+    ]),
+    "UIStatusBarStyle": .string("UIStatusBarStyleDefault"),
+    "UIViewControllerBasedStatusBarAppearance": .boolean(true),
+    "UIRequiresFullScreen": .boolean(false),
+    "OneSignal_suppress_launch_urls": .boolean(true),
+    "BGTaskSchedulerPermittedIdentifiers": .array([
+        .string("$(PRODUCT_BUNDLE_IDENTIFIER)"),
+    ]),
+    "ITSAppUsesNonExemptEncryption": .boolean(false),
+    "LSApplicationQueriesSchemes": .array([
+    ]),
+    "LSEnvironment": .dictionary([:
+                                    
+                                 ]),
+    "NSBonjourServices": .array([
+        .string("_Proxyman._tcp"),
+    ]),
+    "UIBackgroundModes": .array([
+        .string("processing"),
+        .string("remote-notification"),
+    ]),
+])
+
+let settings: SettingsDictionary = [
+    "PRODUCT_NAME": .string("Walnut"),
+    "DISPLAY_NAME": .string("Walnut"),
+    "PRODUCT_BUNDLE_IDENTIFIER": .string("m.walnut"),
+    "EXECUTABLE_NAME": .string("Walnut - Health Tracker"),
+    "CURRENT_PROJECT_VERSION": .string("1.0"),
+    "MARKETING_VERSION": "1",
+    "OTHER_LDFLAGS": "-ObjC",
+]
+    .automaticCodeSigning(devTeam: "Q7HVAVTGUP")
+
 let project = Project(
     name: "Walnut",
+    organizationName: "m",
+    settings: .settings(
+        defaultSettings: .recommended
+    ),
     targets: [
         .target(
-            name: "Walnut",
+            name: "Walnut - Health Tracker",
             destinations: .iOS,
             product: .app,
-            bundleId: "io.tuist.Walnut",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
-            sources: ["Walnut/Sources/**"],
-            resources: ["Walnut/Resources/**"],
-            dependencies: []
+            bundleId: "m.walnut",
+            infoPlist: infoPlist,
+            sources: ["Sources/**/*"],
+            resources: [
+                "Resources/**/*",
+            ],
+            entitlements: .file(path: .relativeToRoot("Walnut/Walnut.entitlements")),
+            dependencies: [
+                .external(name: "ComposableArchitecture"),
+                .external(name: "PostHog"),
+            ],
+            settings: .settings(base: settings,
+                                configurations: [
+                                    .debug(name: "Debug"),
+                                    .release(name: "Release"),
+                                ],
+                                defaultSettings: .recommended)
         ),
         .target(
             name: "WalnutTests",
--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,22 +1,16 @@
 // swift-tools-version: 6.0
-import PackageDescription
-
-#if TUIST
-    import struct ProjectDescription.PackageSettings
-
-    let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,]
-        productTypes: [:]
-    )
-#endif
+@preconcurrency import PackageDescription
 
 let package = Package(
-    name: "Walnut",
+    name: "WalnutDependencies",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v17)],
     dependencies: [
-        // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        // Architecture and State Management
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.19.1"),
+        
+        // Analytics
+        .package(url: "git@github.com:PostHog/posthog-ios.git", from: "3.25.0"),
+
     ]
 )
